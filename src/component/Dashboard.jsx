f20f5f86e18eaec76a71733ccf6078ec52ba3868
bintangithub <bintangithub@gmail.com>
2024-12-14 08:21:05 +0700

    update Dashboard

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -19,7 +19,11 @@ function Dashboard() {
         <div className="dashboard section">
             <div className="row">
                 <div className="col-lg-8">
-                    <div className="row"></div>
+                    <div className="row">
+                        {
+                            cards  
+                        }
+                    </div>
                 </div>
                 <div className="col-lg-4"></div>
             </div>
